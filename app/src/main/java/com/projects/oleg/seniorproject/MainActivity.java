92c472743aac81894f1d7b7d2624bbbf95e0396b
oleg@laptop <o.tolstov1569@gmail.com>
2015-10-08 16:14:12 -0700

    initial commit

---
+++ app/src/main/java/com/projects/oleg/seniorproject/MainActivity.java
@@ -0,0 +1,11 @@
+package com.projects.oleg.seniorproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
